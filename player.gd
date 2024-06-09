c2353014fb553df7b031ef998663eef5eabe197b
Nathan McClain <n8mcclain@gmail.com>
2024-06-09 01:22:09 -0400

    Added Lifebar with color change

--- player.gd
+++ player.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
 
+var health = 100.0
+
+signal health_depleted
+
 # Basic Character movement using Input Map
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -19,3 +23,18 @@ func _physics_process(delta):
 	# selecting `Access by unique name` it can then be accessed as 
 	# `%HappyBoo`.  It is only accessible from the current scene.
 	###
+	
+	const DAMAGE_RATE = 5.0
+	var overlapping_mobs = %hurtbox.get_overlapping_bodies()
+	if overlapping_mobs.size() > 0:
+		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+		%ProgressBar.value = health
+		
+		if health <= 0.0:
+			health_depleted.emit()
+		elif health <= 20.0: #change color when at low health
+			%ProgressBar.get("theme_override_styles/fill").bg_color = Color(1,0,0,1)
+		elif health <= 50.0:
+			%ProgressBar.get("theme_override_styles/fill").bg_color = Color.YELLOW
+		else:
+			%ProgressBar.get("theme_override_styles/fill").bg_color = Color(0.62, 1.0, 0.31, 1.0)
