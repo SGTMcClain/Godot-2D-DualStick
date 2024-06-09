be025156e43d6c2f5dd67933f0f918124930ccb8
Nathan McClain <n8mcclain@gmail.com>
2024-06-09 00:35:03 -0400

    Working auto gun and mob damage

--- mob.gd
+++ mob.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-
+var health = 3
 # Get reference to the player
 #var player
 
@@ -10,7 +10,21 @@ extends CharacterBody2D
 # or use the shorthand
 @onready var player = get_node("/root/Game/Player")
 
+func _ready():
+	%Slime.play_walk()
+
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 300.0
 	move_and_slide()
+
+func take_damage():
+	health -= 1
+	%Slime.play_hurt
+	
+	if health == 0:
+		queue_free()
+		const SMOKE_SCENE = preload("res://smoke_explosion/smoke_explosion.tscn")
+		var smoke = SMOKE_SCENE.instantiate()
+		get_parent().add_child(smoke) # make the smoke a sibling of the mob so persists 
+		smoke.global_position = global_position # the global position of the mob
